0bf5b70430d0bb0163ace8eead783d8b0aa56c58
vrtineu <vininro@gmail.com>
2024-09-22 15:48:23 -0300

    add genstage lib

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule PocPurchase.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:gen_stage, "~> 1.0"}
     ]
   end
 
